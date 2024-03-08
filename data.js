5aadfd61c7b3215440094d8abeb58c1d019e570a
Rafia1996 <161371012+Rafia1996@users.noreply.github.com>
2024-03-08 23:00:05 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+// Data types
+// Example 1 String 
+var fulName = "RAFIA OWAIS";
+console.log(fulName);
+// Example 2 Number
+var age = 28;
+console.log(age);
+// Example 3 Boolean
+var isStudent = true;
+console.log(isStudent);
+// Example 4 Null
+var a = null;
+console.log(a);
+//Example  5 Undefined
+var b = undefined;
+console.log(b);
+// Example  6 BigInt
+var bigNumber = 567894328n;
+console.log(bigNumber);
+//Example 7 Symbol
+var uniqueSymbol = Symbol('Hello');
+console.log(uniqueSymbol);
---
+++ data.ts
@@ -0,0 +1,29 @@
+// Data types
+
+// Example 1 String 
+let fulName : string = "RAFIA OWAIS";
+console.log(fulName);
+
+// Example 2 Number
+let age : number = 28;
+console.log(age);
+
+// Example 3 Boolean
+let isStudent : boolean = true;
+console.log(isStudent);
+
+// Example 4 Null
+let a : null = null;
+console.log(a);
+
+//Example  5 Undefined
+let b : undefined = undefined;
+console.log(b);
+
+// Example  6 BigInt
+var bigNumber : bigint =11233456788n;
+console.log(bigNumber);
+
+//Example 7 Symbol
+let uniqueSymbol = Symbol('Hello');
+console.log(uniqueSymbol);
